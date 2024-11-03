746f6a3f220d11a78063d999798b03f3243e31ab
Salman21jama <salman0sman091@gmail.com>
2024-11-03 07:28:24 +0300

    first change

--- controllers/taskcontroller.js
+++ controllers/taskcontroller.js
@@ -2,6 +2,8 @@ const {IncomingForm}= require('formidable');
 const { readTasksFromFile, writeTasksToFile } = require("../utils/fileHandler")
 const {copyFileSync}=require('fs');
 
+const filePath = './data/tasks.json';
+
 exports.getTasks= (req ,res)=>{
     const tasks= readTasksFromFile(); 
     res.writeHead(200 , {'content-type':'application/json'})
